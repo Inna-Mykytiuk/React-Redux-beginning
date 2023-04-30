d56d481cb50ba8de7c5f0408e2eecab2276bd44c
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-04-30 19:44:36 +0300

    add pictures

--- src/components/AppBar/AppBar.styled.jsx
+++ src/components/AppBar/AppBar.styled.jsx
@@ -7,21 +7,25 @@ export const Wrapper = styled.div`
   justify-content: center;
 
   align-items: center;
-  color: rgb(255, 255, 255);
+  /* color: rgb(24 13 13); */
+  color: #fff;
   backdrop-filter: blur(20px);
   background-color: #5a48483b;
+
   padding: 20px 30px;
   border-radius: 10px;
 `;
 
 export const Section = styled.section`
-  display: grid;
-  gap: 4px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 export const MainTitle = styled.h1`
   font-size: 36px;
   font-weight: 700;
+  /* color: rgb(48, 20, 20); */
   color: #fff;
   text-align: center;
   margin: 0;
@@ -32,4 +36,16 @@ export const MainTitle = styled.h1`
 export const Title = styled.h2`
   margin: 0;
   font-size: 16px;
+  text-shadow: -1px -1px 1px rgba(255, 255, 255, 0.1),
+    1px 1px 1px rgba(0, 0, 0, 0.5);
+`;
+
+export const Img = styled.img`
+  width: 70%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+  transition: 0.5s ease-in-out;
+
+  margin: 10px 0;
 `;
--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -5,18 +5,27 @@ export const StyledButton = styled.button`
   padding: 8px 12px;
   border-radius: 4px;
   border: none;
-  font-size: inherit;
+
+  font-size: 16px;
+  font-weight: 700;
+  text-shadow: rgba(255, 255, 255, 0.1) -1px -1px 1px,
+    rgba(0, 0, 0, 0.5) 1px 1px 1px;
+
   cursor: pointer;
-  background-color: ${({ selected }) => (selected ? '#1976d2' : '#e2e5e8')};
+  background-color: ${({ selected }) =>
+    selected ? 'rgb(215 67 67)' : 'rgb(229 188 188)'};
   color: ${({ selected }) => (selected ? '#fff' : 'inherit')};
 
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 3px 6px;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
   &.isSelected {
-    background-color: #1976d2;
+    background-color: rgb(215 67 67);
     color: #fff;
   }
 
   &:hover {
-    background-color: #1976d2;
+    background-color: rgb(215 67 67);
     color: #fff;
   }
 `;
